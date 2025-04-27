dabd69024019d8ea2c3bb91c3c78d8a11d73659c
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-04-27 17:38:04 -0400

    Add move updates

---
+++ data/mods/polishedcrystal/moves.ts
@@ -0,0 +1,138 @@
+export const Moves: {[k: string]: ModdedMoveData} = {
+    cut: {
+        inherit: true,
+        accuracy: 100,
+        basePower: 60,
+        type: "Steel",
+    },
+    fly: {
+        inherit: true,
+        accuracy: 100,
+    },
+    takedown: {
+        inherit: true,
+        accuracy: 100,
+        pp: 15,
+    },
+    sing: {
+        inherit: true,
+        accuracy: 75,
+    },
+    supersonic: {
+        inherit: true,
+        accuracy: 65,
+    },
+    strength: {
+        inherit: true,
+        type: "Fighting",
+    },
+    firespin: {
+        inherit: true,
+        basePower: 40,
+        accuracy: 90,
+    },
+    dig: {
+        inherit: true,
+        basePower: 90,
+    },
+    scald: {
+        inherit: true,
+        basePower: 70,
+    },
+    crabhammer: {
+        inherit: true,
+        accuracy: 95,
+    },
+    furyswipes: {
+        inherit: true,
+        name: "Fury Strikes",
+        basePower: 20,
+        accuracy: 85,
+    },
+    octazooka: {
+        inherit: true,
+        basePower: 75,
+        accuracy: 95,
+    },
+    painsplit: {
+        inherit: true,
+        type: "Ghost",
+    },
+    rapidspin: {
+        inherit: true,
+        pp: 20,
+    },
+    hiddenpower: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerbug: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerdark: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerdragon: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerelectric: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerfighting: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerfire: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerflying: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerghost: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowergrass: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerground: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerice: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerpoison: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerpsychic: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerrock: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowersteel: {
+        inherit: true,
+        basePower: 70,
+    },
+    hiddenpowerwater: {
+        inherit: true,
+        basePower: 70,
+    },
+    whirlpool: {
+        inherit: true,
+        basePower: 40,
+        accuracy: 90,
+    }
+};
