1941b960d17123c685a1eb24cd3c6b424e652765
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-06-04 21:54:18 +0800

    Fix dex moves
    Specify all obtainable + unobtainable

--- data/mods/polishedcrystal/rulesets.ts
+++ data/mods/polishedcrystal/rulesets.ts
@@ -3,8 +3,8 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
         inherit: true,
 		ruleset: [
 			// Standard rules
-			'Obtainable', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Evasion Clause', 'Endless Battle Clause',
-			'HP Percentage Mod', 'Cancel Mod',
+			'Obtainable', '+Past', 'Sketch Post-Gen 7 Moves', 'Species Clause', 'Nickname Clause', 'OHKO Clause',
+			'Evasion Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod',
 			// Polished specific
 			'Sleep Clause Mod', 'Freeze Clause Mod', 'Terastal Clause', 'DryPass Clause',
 		],
