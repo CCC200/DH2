f787abe2982e7e37200b4508a4ae8e3217bb2aec
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-06-13 09:37:00 -0400

    Update Berserk Gene desc for polished
    Just to remove (Gen 2) from the beginning

---
+++ data/mods/polishedcrystal/items.ts
@@ -0,0 +1,6 @@
+export const Items: {[k: string]: ModdedItemData} = {
+    berserkgene: {
+        inherit: true,
+        shortDesc: "On switch-in, raises holder's Attack by 2 and confuses it. Single use.",
+    },
+};
