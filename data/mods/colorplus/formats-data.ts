434fd1fb9b4892708a93dfb2fb6d77216f4c148b
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-06-14 11:52:17 -0400

    Add colorplus tiering

---
+++ data/mods/colorplus/formats-data.ts
@@ -0,0 +1,82 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	// Updated RBY mons
+	chansey: {
+		tier: "UUBL",
+	},
+	persian: {
+		tier: "OU",
+	},
+	hypno: {
+		tier: "OU",
+	},
+	slowbro: {
+		tier: "UU",
+	},
+	lapras: {
+		tier: "UUBL",
+	},
+	nidoking: {
+		tier: "UUBL",
+	},
+	nidoqueen: {
+		tier: "UUBL",
+	},
+	moltres: {
+		tier: "UUBL",
+	},
+	vaporeon: {
+		tier: "UUBL",
+	},
+	jolteon: {
+		tier: "OU"
+	},
+	articuno: {
+		tier: "UUBL",
+	},
+	golem: {
+		tier: "UUBL",
+	},
+	victreebel: {
+		tier: "NU",
+	},
+	dragonite: {
+		tier: "UUBL",
+	},
+	clefable: {
+		tier: "UUBL",
+	},
+	// GSC evolutions
+	crobat: {
+		tier: "UU",
+	},
+	blissey: {
+		tier: "OU",
+	},
+	kingdra: {
+		tier: "UUBL",
+	},
+	espeon: {
+		tier: "OU",
+	},
+	umbreon: {
+		tier: "OU",
+	},
+	porygon2: {
+		tier: "OU",
+	},
+	bellossom: {
+		tier: "UU",
+	},
+	politoed: {
+		tier: "UUBL",
+	},
+	steelix: {
+		tier: "OU",
+	},
+	slowking: {
+		tier: "OU",
+	},
+	scizor: {
+		tier: "UUBL",
+	},
+};
