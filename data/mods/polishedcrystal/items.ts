e13a16f7db95ba29c18f0a123988475375719592
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-06-14 08:35:40 -0400

    Item fixes

--- data/mods/polishedcrystal/items.ts
+++ data/mods/polishedcrystal/items.ts
@@ -3,4 +3,8 @@ export const Items: {[k: string]: ModdedItemData} = {
         inherit: true,
         shortDesc: "On switch-in, raises holder's Attack by 2 and confuses it. Single use.",
     },
+    pinkbow: {
+        inherit: true,
+        shortDesc: "Holder's Normal-type attacks have 1.1x power.",
+    },
 };
