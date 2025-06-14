72335ea0fc21f540779ec11f0d076f990753c12b
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-06-14 09:26:36 -0400

    Fix colorplus type/move imports

--- data/mods/colorplus/moves.ts
+++ data/mods/colorplus/moves.ts
@@ -3,6 +3,12 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
         type: "Dark",
 		category: "Special",
+		secondary: {
+			chance: 10,
+			volatileStatus: 'flinch',
+		},
+		desc: "Has a 10% chance to make the target flinch.",
+		shortDesc: "10% chance to make the target flinch.",
 	},
     karatechop: {
 		inherit: true,
