89261b0c894c3135d9ba634f0ab91dc7118f109a
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-06-24 16:07:33 -0400

    isFinals var for rated

--- data/mods/gen3draft/rulesets.ts
+++ data/mods/gen3draft/rulesets.ts
@@ -1,6 +1,10 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
     standarddraft: {
         inherit: true,
+        onBegin() {
+            const isFinals = false;
+            if(isFinals) this.add('rated', 'Tournament Finals');
+        },
         onValidateTeam(team) {
             const tiers = ["S", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
             for(const set of team) {
