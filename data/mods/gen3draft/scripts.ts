b88fa8c807ab37403f6598cd9207537c7bd9307b
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-07-20 15:45:58 -0400

    Fix draft ability list

--- data/mods/gen3draft/scripts.ts
+++ data/mods/gen3draft/scripts.ts
@@ -1,5 +1,11 @@
-export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen3',
+	init() {
+		// Fix ability list
+		for(const i in this.data.Pokedex) {
+			this.modData('Pokedex', i).abilities = this.species.get(i).abilities;
+		}
+	},
 	teambuilderConfig: {
 		excludeStandardTiers: true,
 		customTiers: ["S", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"],
