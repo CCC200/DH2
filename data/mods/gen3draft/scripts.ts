dfe3621df3de6f3b9128e33512550556d6cb99e1
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-07-21 16:10:12 -0400

    Fix draft move desc

--- data/mods/gen3draft/scripts.ts
+++ data/mods/gen3draft/scripts.ts
@@ -1,9 +1,14 @@
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen3',
 	init() {
-		// Fix ability list
+		// Fix ability list & move desc
 		for(const i in this.data.Pokedex) {
 			this.modData('Pokedex', i).abilities = this.species.get(i).abilities;
+		}
+		for(const i in this.data.Moves) {
+			const move = this.moves.get(i);
+			this.modData('Moves', i).desc = move.desc;
+			this.modData('Moves', i).shortDesc = move.shortDesc;
 		}
 	},
 	teambuilderConfig: {
