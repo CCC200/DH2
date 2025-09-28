f9fcbebc24b32c2d92fb258cd68a96901841437f
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-09-27 21:02:34 -0400

    Polished fix hail-related ability descriptions

--- data/mods/polishedcrystal/abilities.ts
+++ data/mods/polishedcrystal/abilities.ts
@@ -6,4 +6,18 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		},
         shortDesc: "On switch-in, this Pokemon summons Hail.",
 	},
+	icebody: {
+		inherit: true,
+		desc: "If Hail is active, this Pokemon restores 1/16 of its maximum HP, rounded down, at the end of each turn. This Pokemon takes no damage from Hail.",
+		shortDesc: "If Hail is active, this Pokemon heals 1/16 of its max HP each turn; immunity to Hail.",
+	},
+	snowcloak: {
+		inherit: true,
+		desc: "If Hail is active, the accuracy of moves used against this Pokemon is multiplied by 0.8. This Pokemon takes no damage from Hail.",
+		shortDesc: "If Hail is active, this Pokemon's evasiveness is 1.25x; immunity to Hail.",
+	},
+	slushrush: {
+		inherit: true,
+		shortDesc: "If Hail is active, this Pokemon's Speed is doubled.",
+	},
 };
